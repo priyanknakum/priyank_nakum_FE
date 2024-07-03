c27c119b253b5e0f84542a7b8fdec349d1e88afc
Priyank Nakum <156404260+priyanknakum@users.noreply.github.com>
2024-07-03 22:11:08 +0530

    03-07-2024

---
+++ Assesment/Assesment-3/script.js
@@ -0,0 +1,6 @@
+let icon = document.getElementById("menubar");
+let menu = document.getElementById("menu");
+function show() {
+  icon.classList.toggle("fa-xmark");
+  menu.classList.toggle("active");
+}
